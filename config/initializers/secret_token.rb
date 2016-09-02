fa4b5d2b9a7c5d369d0e172f0dd7e136194edbbf
Mason <lantay@alumni.unc.edu>
2016-09-02 23:05:05 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixtopia::Application.config.secret_key_base = '670f036c2cebc7a315c5edcb7fa1d3d8a86e5251e5eba45d8d56ddaf64e832740faf2e73c5cb540637cd6553bae4dc7c20d85a9d4e4e6de81cfd0f7fd48b712f'
